e64da8fab549c1503277337090b25be338d23642
Bero <berislav.grgicak@gmail.com>
2023-03-01 20:31:39 +0100

    Add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import vuetify from "./plugins/vuetify";
+
 import "./assets/main.scss";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import vuetify from 'vite-plugin-vuetify'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-console.log(import.meta);
+
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({ styles: { configFile: 'src/styles/vuetify-settings.scss' } }),
     VitePWA({
       injectRegister: 'auto',
       workbox: {
