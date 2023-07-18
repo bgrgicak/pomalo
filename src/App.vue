532ea65f1671d9123034babd7d3524398d77daa3
Bero <berislav.grgicak@gmail.com>
2023-07-18 18:22:51 +0200

    Move layout to components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import Header from "./layout/Header.vue";
-import Navigation from "./layout/Navigation.vue";
-import LeftSidebar from "./layout/LeftSidebar.vue";
-import RightSidebar from "./layout/RightSidebar.vue";
-import Main from "./layout/Main.vue";
+import Header from "./components/layout/Header.vue";
+import Navigation from "./components/layout/Navigation.vue";
+import LeftSidebar from "./components/layout/LeftSidebar.vue";
+import RightSidebar from "./components/layout/RightSidebar.vue";
+import Main from "./components/layout/Main.vue";
 </script>
 
 <template>
