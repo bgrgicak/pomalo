aa8dc190e770ab0e8392128e025a137b47ce9755
Bero <berislav.grgicak@gmail.com>
2023-07-23 15:49:21 +0200

    hide right sidebar on outside click

--- src/components/layout/RightSidebar.vue
+++ src/components/layout/RightSidebar.vue
@@ -5,15 +5,24 @@ import { computed } from 'vue';
 
 const layoutStore = useLayoutStore();
 const activity = computed(() => layoutStore.currentActivity);
+const hide = () => {
+    layoutStore.hideRightSidebar();
+};
 </script>
 <template>
-    <v-navigation-drawer location="right" v-if="layoutStore.isRightSidebarVisible" class="right-sidebar">
-        <ActivityDetails :activity="activity" small="true"/>
+    <v-navigation-drawer location="right"
+                         v-if="layoutStore.isRightSidebarVisible"
+                         class="right-sidebar">
+        <div v-click-outside="hide">
+            <ActivityDetails :activity="activity"
+                             small="true" />
+        </div>
     </v-navigation-drawer>
 </template>
 <style scoped lang="scss">
-    @import '@/assets/styles/variables.scss';
-    .right-sidebar {
-        top: $header-height !important;
-    }
+@import '@/assets/styles/variables.scss';
+
+.right-sidebar {
+    top: $header-height !important;
+}
 </style>
