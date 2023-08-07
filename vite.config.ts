ae58e55e47060f74079dfe21b39b09af244b63f7
Bero <berislav.grgicak@gmail.com>
2023-08-07 12:25:44 +0200

    Update manifest build

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,23 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       },
+      manifest: {
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
+      }
     })
   ],
   resolve: {
