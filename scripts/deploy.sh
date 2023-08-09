6db7f95ba758fabcfa5701612c3c1e0f49cb2240
Bero <berislav.grgicak@gmail.com>
2023-08-09 14:16:28 +0200

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
+!/bin/bash
+
+CURRENT_BRANCH=$(git branch --show-current)
+
 git checkout production
 git pull
-git merge main
+git merge $CURRENT_BRANCH
 
 npm run build
 
 git add -f public
 git commit -m "Deploying to production"
 git push
+
+git checkout $CURRENT_BRANCH
