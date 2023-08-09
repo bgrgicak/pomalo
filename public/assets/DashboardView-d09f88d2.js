c3b1adb0362a7b08a053881e7cca1f72dd926e9b
Bero <berislav.grgicak@gmail.com>
2023-08-09 14:13:22 +0200

    Deploying to production

---
+++ public/assets/DashboardView-d09f88d2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Calendar.vue_vue_type_style_index_0_lang-5a8145cd.js";import{d as a,o,c as n,a as r}from"./index-345190df.js";const p=a({__name:"DashboardView",setup(t){return(s,c)=>(o(),n("main",null,[r(e)]))}});export{p as default};
---
+++ public/assets/SettingsView-8920ee3e.js
@@ -0,0 +1,1 @@
+import{am as o,x as a,o as s,b as n,w as e,a as c,f as r,ao as _,k as l}from"./index-345190df.js";const i={},d=l("span",null,"Settings",-1);function f(u,m){const t=a("v-title");return s(),n(t,null,{default:e(()=>[c(_,null,{default:e(()=>[r("mdi-cog")]),_:1}),d]),_:1})}const x=o(i,[["render",f]]);export{x as default};
