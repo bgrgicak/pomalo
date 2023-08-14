6e5c309d97699b628a69f73f35f27d36ffd52e91
Bero <berislav.grgicak@gmail.com>
2023-08-14 17:41:00 +0200

    Update deploy to use main

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,19 @@
 
 CURRENT_BRANCH=$(git branch --show-current)
 
+echo "Deploying MAIN branch to production"
+
+git checkout main
+git pull
+
 git checkout production
 git pull
-git merge $CURRENT_BRANCH
+git merge main
 
 npm run build
 
 git add -f public
-git commit --amend -m "Deploying to production"
+git commit --no-verify -m "Deploying to production"
 git push
 
 git checkout $CURRENT_BRANCH
