4dc757ea7281c349611d24565bf5aa1dced98ad4
Bero <berislav.grgicak@gmail.com>
2023-08-14 17:42:38 +0200

    Skip merge message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,12 @@ git pull
 
 git checkout production
 git pull
-git merge main
+git merge --no-edit main
 
 npm run build
 
 git add -f public
-git commit --no-verify -m "Deploying to production"
+git commit -m "Deploying to production"
 git push
 
 git checkout $CURRENT_BRANCH
