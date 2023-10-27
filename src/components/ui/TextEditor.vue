93b7e09d8dcb3401aeee68abf40436b15ce29064
Bero <berislav.grgicak@gmail.com>
2023-10-27 07:12:49 +0200

    Pell editor

--- src/components/ui/TextEditor.vue
+++ src/components/ui/TextEditor.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import __ from '@/helper/translations';
 import { ref, type PropType, type Ref } from 'vue';
+import pell from 'pell';
+import { watch } from 'vue';
 
 const props = defineProps({
 	modelValue: {
@@ -21,12 +23,59 @@ const emit = defineEmits(['update:modelValue']);
 const focused: Ref<boolean> = ref(false);
 const editor: Ref<any> = ref(null);
 const textEditorRef: Ref<any> = ref(null);
-// Prevent value changes from triggering an update
-const value = props.modelValue;
 
-const onChange = (payload: any) => {
-	emit('update:modelValue', payload?.originalTarget.innerHTML ?? '');
-};
+watch(
+	editor,
+	() => {
+		if (!editor.value) {
+			return;
+		}
+		const peelEditor = pell.init({
+			element: editor.value,
+			onChange: (html: string) => {
+				emit('update:modelValue', html);
+			},
+			defaultParagraphSeparator: 'p',
+			styleWithCSS: true,
+			actions: [
+				'bold',
+				'underline',
+				{
+					name: 'italic',
+					result: () => pell.exec('italic')
+				},
+				{
+					name: 'image',
+					result: () => {
+						const url = window.prompt('Enter the image URL');
+						if (url) pell.exec('insertImage', url);
+					}
+				},
+				{
+					name: 'link',
+					result: () => {
+						const url = window.prompt('Enter the link URL');
+						if (url) pell.exec('createLink', url);
+					}
+				}
+			],
+			classes: {
+				actionbar: 'v-btn-group v-btn-group--density-default v-btn-toggle mb-4',
+				button: 'v-btn v-btn--flat v-btn--icon v-btn--density-default v-btn--size-default v-btn--variant-outlined',
+				content: 'texteditor__content',
+				selected: 'v-btn--active'
+			}
+		});
+		peelEditor.content.innerHTML = props.modelValue;
+		peelEditor.content.addEventListener('focus', () => {
+			focused.value = true;
+		});
+		peelEditor.content.addEventListener('blur', () => {
+			focused.value = false;
+		});
+	},
+);
+
 const focusEditor = () => {
 	if (editor.value) {
 		editor.value.focus();
@@ -41,25 +90,24 @@ const focusEditor = () => {
     :focused="focused"
     @click="focusEditor"
   >
-    <!-- eslint-disable vue/no-v-html -->
-    <div
-      class="text-editor__inner"
-      :contenteditable="!props.readonly"
-      @keyup="onChange"
-      @focus="focused = true"
-      @blur="focused = false"
-      v-html="value"
+    <pre
+      ref="editor"
+      class="texteditor__inner"
     />
-    <!--eslint-enable-->
   </v-input>
 </template>
 <style lang="scss">
-
+$texeditor-height: 400px;
 .text-editor {
     border: thin solid rgba(var(--v-border-color), var(--v-disabled-opacity));
 	.v-input__control {
 		padding: 1rem;
         border: 2px solid transparent;
+	}
+
+
+	.pell-actionbar {
+		display: none;
 	}
     &:hover,
     &.text-editor--focused  {
@@ -69,11 +117,15 @@ const focusEditor = () => {
 		.v-input__control {
         	border-color: rgba(var(--v-border-color), var(--v-field-border-opacity));
 		}
+		.pell-actionbar {
+			display: flex;
+		}
     }
-	textarea {
+	.texteditor__inner,
+	.texteditor__content {
 		font-size: 1rem;
 		width: 100%;
-		min-height: 200px;
+		min-height: $texeditor-height;
 		outline: none;
 	}
 	.v-input__details {
