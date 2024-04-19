f94f850bf7b92db188fce8df9fdb67dae2a9d26e
bgrgicak <berislav.grgicak@gmail.com>
2024-04-19 12:45:54 +0200

    Watch for new calendar events

--- src/stores/calendar.ts
+++ src/stores/calendar.ts
@@ -2,151 +2,175 @@ import { parseEventsFromActivities } from '@/data/events';
 import { getUtcTimestamp } from '@/helper/date';
 import type { CalendarClipboard, CalendarClipboardType, CalendarEvent, CalendarState } from '@/types/calendar';
 import { defineStore } from 'pinia';
-import { computed, ref, type Ref, watch } from 'vue';
+import { computed, ref, type Ref } from 'vue';
 import { useActivityStore } from './activities';
 import { useTimerStore } from './timer';
+import database from '@/data/pouchdb';
+import log from '@/helper/logs';
+import type { ActivityDocument } from '@/types/activity-document';
+import { parseDocumentToActivity } from '@/data/activities';
+import type Activity from '../types/activity';
 
-export const useCalendarStore = defineStore(
-	'calendar',
-	() => {
-		const state: Ref<CalendarState> = ref({
-			activityIds: [],
-			focusedEvent: undefined,
-			focusedCell: undefined,
-			loading: true,
-			clipboard: undefined,
-		});
+export const useCalendarStore = defineStore('calendar', () => {
+	const state: Ref<CalendarState> = ref({
+		activityIds: [],
+		focusedEvent: undefined,
+		focusedCell: undefined,
+		loading: true,
+		clipboard: undefined,
+	});
 
-		const activityStore = useActivityStore();
-		const timerStore = useTimerStore();
+	const activityStore = useActivityStore();
+	const timerStore = useTimerStore();
 
-		watch(
-			activityStore.list,
-			(activities) => {
-				if ( !state.value.startTime || !state.value.endTime ) {
-					return;
-				}
-				for (const activity of activities) {
-					if (
-						(
-							activity.eventFirstStart &&
-							activity.eventFirstStart >= state.value.endTime
-						) ||
-						(
-							activity.eventLastEnd &&
-							activity.eventLastEnd <= state.value.startTime
-						)
-					) {
-						addActivityId(activity._id);
-					}
-				}
-			}
-		);
+	const maybeAddActivity = (activity: Activity) => {
+		if (!state.value.startTime || !state.value.endTime) {
+			return;
+		}
+		if (
+			(activity.eventFirstStart &&
+				activity.eventFirstStart <= state.value.endTime) ||
+			(activity.eventLastEnd &&
+				activity.eventLastEnd >= state.value.startTime)
+		) {
+			addActivityId(activity._id);
+		}
+	};
 
-		const isLoading = computed((): boolean => state.value.loading);
-		const hasNewEvent = computed((): boolean => !!state.value.newEvent);
-		const focusedEvent = computed((): CalendarEvent | undefined => state.value.focusedEvent);
-		const clipboard = computed((): CalendarClipboard | undefined => state.value.clipboard);
-		const focusedCell = computed((): Date | undefined => state.value.focusedCell);
-		const events = computed((): CalendarEvent[] => {
-			if (!state.value.startTime || !state.value.endTime) {
-				return [];
-			}
-			const events = parseEventsFromActivities(
-				activityStore.list.filter((activity) => {
-					return state.value.activityIds.includes(activity._id as string);
-				}),
-				state.value.startTime,
-				state.value.endTime,
-				timerStore.activityId,
+	database
+		.changes({
+			since: 'now',
+			live: true,
+			include_docs: true,
+		})
+		.on('change', (change) => {
+			maybeAddActivity(
+				parseDocumentToActivity(change.doc as ActivityDocument)
 			);
-			if (state.value.newEvent) {
-				events.push(state.value.newEvent);
-			}
-			return events;
+		})
+		.catch((error) => {
+			log(error);
 		});
 
-		const load = (start: Date, end: Date) => {
-			state.value.startTime = start;
-			state.value.endTime = end;
-			return activityStore.find({
-				'selector': {
-					'eventFirstStart': {
-						'$lte': getUtcTimestamp(state.value.endTime)
+	const isLoading = computed((): boolean => state.value.loading);
+	const hasNewEvent = computed((): boolean => !!state.value.newEvent);
+	const focusedEvent = computed(
+		(): CalendarEvent | undefined => state.value.focusedEvent
+	);
+	const clipboard = computed(
+		(): CalendarClipboard | undefined => state.value.clipboard
+	);
+	const focusedCell = computed(
+		(): Date | undefined => state.value.focusedCell
+	);
+	const events = computed((): CalendarEvent[] => {
+		if (!state.value.startTime || !state.value.endTime) {
+			return [];
+		}
+		const events = parseEventsFromActivities(
+			activityStore.list.filter((activity) => {
+				return state.value.activityIds.includes(activity._id as string);
+			}),
+			state.value.startTime,
+			state.value.endTime,
+			timerStore.activityId
+		);
+		if (state.value.newEvent) {
+			events.push(state.value.newEvent);
+		}
+		return events;
+	});
+
+	const load = (start: Date, end: Date) => {
+		state.value.startTime = start;
+		state.value.endTime = end;
+		return activityStore
+			.find({
+				selector: {
+					eventFirstStart: {
+						$lte: getUtcTimestamp(state.value.endTime),
 					},
-					'eventLastEnd': {
-						'$gte': getUtcTimestamp(state.value.startTime)
+					eventLastEnd: {
+						$gte: getUtcTimestamp(state.value.startTime),
 					},
 				},
-			}).then((activities) => {
-				state.value.activityIds = activities ? activities.map((activity) => activity._id as string) : [];
+			})
+			.then((activities) => {
+				state.value.activityIds = activities
+					? activities.map((activity) => activity._id as string)
+					: [];
 				return state.value.activityIds;
 			});
-		};
+	};
 
-		const addActivityId = (activityId: string) => {
-			if (!state.value.activityIds.includes(activityId)) {
-				state.value.activityIds.push(activityId);
-			}
-		};
+	const addActivityId = (activityId: string) => {
+		if (!state.value.activityIds.includes(activityId)) {
+			state.value.activityIds.push(activityId);
+		}
+	};
 
-		const addNewEvent = (event: CalendarEvent) => {
-			state.value.newEvent = event;
-			if (event.id) {
-				addActivityId(event.id);
-			}
-		};
+	const addNewEvent = (event: CalendarEvent) => {
+		state.value.newEvent = event;
+		if (event.id) {
+			addActivityId(event.id);
+		}
+	};
 
-		const removeNewEvent = () => {
-			state.value.newEvent = undefined;
-		};
+	const removeNewEvent = () => {
+		state.value.newEvent = undefined;
+	};
 
-		const focusEvent = (eventId: string) => {
-			state.value.focusedEvent = events.value.find((event) => event.eventId === eventId);
-		};
+	const focusEvent = (eventId: string) => {
+		state.value.focusedEvent = events.value.find(
+			(event) => event.eventId === eventId
+		);
+	};
 
-		const unfocusEvent = () => {
-			state.value.focusedEvent = undefined;
-		};
+	const unfocusEvent = () => {
+		state.value.focusedEvent = undefined;
+	};
 
-		const focusCell = (date: Date) => {
-			state.value.focusedCell = date;
-		};
+	const focusCell = (date: Date) => {
+		state.value.focusedCell = date;
+	};
 
-		const unfocusCell = () => {
-			state.value.focusedCell = undefined;
-		};
+	const unfocusCell = () => {
+		state.value.focusedCell = undefined;
+	};
 
-		const addToClipboard = (activityId: string, eventId: string, type: CalendarClipboardType) => {
-			state.value.clipboard = {
-				activityId,
-				eventId,
-				type,
-			};
+	const addToClipboard = (
+		activityId: string,
+		eventId: string,
+		type: CalendarClipboardType
+	) => {
+		state.value.clipboard = {
+			activityId,
+			eventId,
+			type,
 		};
+	};
 
-		const removeFromClipboard = () => {
-			state.value.clipboard = undefined;
-		};
+	const removeFromClipboard = () => {
+		state.value.clipboard = undefined;
+	};
 
-		return {
-			state,
-			isLoading,
-			events,
-			focusedEvent,
-			focusedCell,
-			clipboard,
-			load,
-			hasNewEvent,
-			addActivityId,
-			addNewEvent,
-			removeNewEvent,
-			focusEvent,
-			unfocusEvent,
-			focusCell,
-			unfocusCell,
-			addToClipboard,
-			removeFromClipboard
-		};
-	}
-);
+	return {
+		state,
+		isLoading,
+		events,
+		focusedEvent,
+		focusedCell,
+		clipboard,
+		load,
+		hasNewEvent,
+		addActivityId,
+		addNewEvent,
+		removeNewEvent,
+		focusEvent,
+		unfocusEvent,
+		focusCell,
+		unfocusCell,
+		addToClipboard,
+		removeFromClipboard,
+	};
+});
